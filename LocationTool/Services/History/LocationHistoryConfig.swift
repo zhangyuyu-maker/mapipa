c1571015e0e3b17237edc34450aa65ee256d33fa
15097485916 <l001198293>
2026-09-22 13:29:42 +0800

    config: API地址改为120.48.137.4:8005

--- LocationTool/Services/History/LocationHistoryConfig.swift
+++ LocationTool/Services/History/LocationHistoryConfig.swift
@@ -1,4 +1,4 @@
-import Foundation
+﻿import Foundation
 
 /// 历史定位 API 配置
 /// 修改 apiBaseURL 即可切换服务器地址 (开发/生产环境)
@@ -7,5 +7,5 @@ enum LocationHistoryConfig {
     /// - 开发环境: http://localhost:3000
     /// - 生产环境: 改为服务器地址, 例如 http://your-server.com:3000
     /// 所有历史记录请求都通过此地址, 方便以后更换服务器
-    static let apiBaseURL = "http://localhost:3000"
+    static let apiBaseURL = "http://120.48.137.4:8005"
 }
